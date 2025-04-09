29f5024ccc451fc03a9c1cca429fbceca5f269c1
m1keee3 <skirdamisa@gmail.com>
2025-04-09 17:07:20 +0300

    add: Wrap func

--- lib/e/e.go
+++ lib/e/e.go
@@ -2,6 +2,10 @@ package e
 
 import "fmt"
 
+func Wrap(msg string, err error) error {
+	return fmt.Errorf("%s: %w", msg, err)
+}
+
 func WrapIfErr(msg string, err error) error {
 	if err != nil {
 		return fmt.Errorf("%s: %w", msg, err)
