ca70fbf0a4de2de982d933862043919f05721a6d
m1keee3 <skirdamisa@gmail.com>
2025-04-10 10:31:43 +0300

    update main

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	tgClient "SPBHistoryBot/clients/telegram"
+	"SPBHistoryBot/consumer/event-consumer"
+	"SPBHistoryBot/events/telegram"
 	"flag"
 	"log"
 )
 
+const (
+	tgBotHost = "api.telegram.org"
+	batchSize = 100
+)
+
 func main() {
+	eventsProcessor := telegram.NewProcessor(tgClient.NewClient(tgBotHost, mustToken()))
+	log.Print("service started")
+
+	consumer := event_consumer.NewConsumer(eventsProcessor, eventsProcessor, batchSize)
+	if err := consumer.Start(); err != nil {
+		log.Fatal("service stopped", err)
+	}
 }
 
 func mustToken() string {
