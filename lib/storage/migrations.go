5615104821617665b134440b6625c2845f55f3cb
m1keee3 <skirdamisa@gmail.com>
2025-04-23 15:24:35 +0300

    add location information

--- lib/storage/migrations.go
+++ lib/storage/migrations.go
@@ -37,36 +37,47 @@ func SeedifEmpty(db *DBStorage) error {
 
 func Seed(db *DBStorage) error {
 
+	peter_fort := Place{
+		Name:      "Петропавловская крепость",
+		Text:      "Форма звезды",
+		Image:     "https://static.78.ru/images/uploads/1686542964143.jpg",
+		Latitude:  59.9500019,
+		Longitude: 30.3166718,
+	}
+
 	is_sobor := Place{
-		Name:  "Исаакиевский собор",
-		Text:  "Построен в 1818–1858 годах",
-		Image: "https://yandex.ru/images/search?pos=5&from=tabbar&img_url=https%3A%2F%2Fsneg.top%2Fuploads%2Fposts%2F2023-03%2F1680058387_sneg-top-p-isaakievskii-sobor-oboi-na-telefon-pintere-26.jpg&text=санкт-петербург&rpt=simage&lr=2",
+		Name:      "Исаакиевский собор",
+		Text:      "Построен в 1818–1858 годах",
+		Image:     "https://yandex.ru/images/search?pos=5&from=tabbar&img_url=https%3A%2F%2Fsneg.top%2Fuploads%2Fposts%2F2023-03%2F1680058387_sneg-top-p-isaakievskii-sobor-oboi-na-telefon-pintere-26.jpg&text=санкт-петербург&rpt=simage&lr=2",
+		Latitude:  59.934111,
+		Longitude: 30.306074,
 	}
 	kaz_sobor := Place{
-		Name:  "Казанский собор",
-		Text:  "Полукруглый",
-		Image: "https://ic.pics.livejournal.com/air_vision/76938106/13174/13174_original.jpg",
+		Name:      "Казанский собор",
+		Text:      "Полукруглый",
+		Image:     "https://ic.pics.livejournal.com/air_vision/76938106/13174/13174_original.jpg",
+		Latitude:  59.93424,
+		Longitude: 30.32444,
 	}
 	spas_na_kr := Place{
-		Name:  "Спас на Крови",
-		Text:  "Красивый",
-		Image: "https://image.fonwall.ru/o/6z/hram-spas-na-krovi-sankt-peterburg-rossiya.jpg?auto=compress&amp;fit=crop&amp;w=2560&amp;h=1440&amp;domain=img.fonwall.ru",
+		Name:      "Спас на Крови",
+		Text:      "Красивый",
+		Image:     "https://image.fonwall.ru/o/6z/hram-spas-na-krovi-sankt-peterburg-rossiya.jpg?auto=compress&amp;fit=crop&amp;w=2560&amp;h=1440&amp;domain=img.fonwall.ru",
+		Latitude:  59.940055,
+		Longitude: 30.328807,
 	}
 	gen_shtab := Place{
-		Name:  "Генеральный штаб",
-		Text:  "Большой",
-		Image: "https://wallpapers.com/images/hd/russia-general-staff-building-ub6k2x9y4ifuadfp.jpg",
-	}
-	peter_fort := Place{
-		Name:  "Петропавловская крепость",
-		Text:  "Форма звезды",
-		Image: "https://static.78.ru/images/uploads/1686542964143.jpg",
+		Name:      "Главный штаб",
+		Text:      "Большой",
+		Image:     "https://wallpapers.com/images/hd/russia-general-staff-building-ub6k2x9y4ifuadfp.jpg",
+		Latitude:  59.933973,
+		Longitude: 30.306103,
 	}
 
 	nevskiy := District{
 		Name:   "Невский район",
 		Text:   "Центральный район города",
-		Places: []Place{is_sobor, kaz_sobor, spas_na_kr, gen_shtab, peter_fort},
+		Places: []Place{peter_fort, is_sobor, kaz_sobor, spas_na_kr, gen_shtab},
 	}
 
 	if err := db.db.Create(&nevskiy).Error; err != nil {
